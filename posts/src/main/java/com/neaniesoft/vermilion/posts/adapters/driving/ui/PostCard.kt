8f2e6fb98212a3f6961eb3b54d90dd9c09665579
Michael Pearce <mdpearce@gmail.com>
2022-02-06 18:49:12 +1100

    Tweak post card layout

--- posts/src/main/java/com/neaniesoft/vermilion/posts/adapters/driving/ui/PostCard.kt
+++ posts/src/main/java/com/neaniesoft/vermilion/posts/adapters/driving/ui/PostCard.kt
@@ -5,7 +5,6 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,7 +15,7 @@ import java.time.Instant
 
 @Composable
 fun PostCard(post: Post, modifier: Modifier = Modifier) {
-    Card(elevation = 8.dp) {
+    Card(elevation = 16.dp) {
         Column(modifier = modifier.padding(16.dp)) {
             when (val summary = post.summary) {
                 is TextPostSummary -> {
@@ -25,17 +24,12 @@ fun PostCard(post: Post, modifier: Modifier = Modifier) {
                 is ImagePostSummary -> TODO()
                 is LinkPostSummary -> TODO()
             }
-
-            Row(
-                verticalAlignment = Alignment.Bottom,
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(
-                    text = post.title.value,
-                    style = MaterialTheme.typography.h4,
-                    modifier = Modifier.padding(top = 8.dp)
-                )
+            Text(
+                text = post.title.value,
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Row(horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth().padding(top = 8.dp)) {
                 Text(text = post.communityName.value, style = MaterialTheme.typography.caption)
             }
         }
@@ -63,7 +57,7 @@ fun PostCardPreview() {
 }
 
 internal val DUMMY_TEXT_POST = Post(
-    PostTitle("Some post"),
+    PostTitle("Some post with a very long title that is likely to split across multiple lines"),
     TextPostSummary(PreviewText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")),
     CommunityName("Subreddit"),
     URL("https://some.url/someicon.png"),
