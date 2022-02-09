d5fcb647adb0da58773afae813e30cc2e3fb357a
Michael Pearce <mdpearce@gmail.com>
2022-02-09 16:51:46 +1100

    Add record repository

--- accounts/src/main/java/com/neaniesoft/vermilion/accounts/adapters/driven/room/UserAccountRoomRecordRepository.kt
+++ accounts/src/main/java/com/neaniesoft/vermilion/accounts/adapters/driven/room/UserAccountRoomRecordRepository.kt
@@ -1,6 +1,23 @@
 package com.neaniesoft.vermilion.accounts.adapters.driven.room
 
+import com.neaniesoft.vermilion.accounts.domain.entities.AuthTokenId
+import com.neaniesoft.vermilion.accounts.domain.entities.UserAccount
+import com.neaniesoft.vermilion.accounts.domain.entities.UserAccountId
 import com.neaniesoft.vermilion.accounts.domain.ports.UserAccountRecordRepository
+import javax.inject.Inject
 
-class UserAccountRoomRecordRepository : UserAccountRecordRepository {
+class UserAccountRoomRecordRepository @Inject constructor(private val userAccountDao: UserAccountDao) :
+    UserAccountRecordRepository {
+    override suspend fun getUserAccountWithId(id: UserAccountId): UserAccount {
+        val dbRecord = userAccountDao.getById(id.value)
+
+        return dbRecord.toUserAccount()
+    }
+}
+
+private fun UserAccountRecord.toUserAccount(): UserAccount {
+    return UserAccount(
+        UserAccountId(id),
+        AuthTokenId(authTokenId)
+    )
 }
--- accounts/src/main/java/com/neaniesoft/vermilion/accounts/domain/ports/UserAccountRecordRepository.kt
+++ accounts/src/main/java/com/neaniesoft/vermilion/accounts/domain/ports/UserAccountRecordRepository.kt
@@ -1,4 +1,8 @@
 package com.neaniesoft.vermilion.accounts.domain.ports
 
+import com.neaniesoft.vermilion.accounts.domain.entities.UserAccount
+import com.neaniesoft.vermilion.accounts.domain.entities.UserAccountId
+
 interface UserAccountRecordRepository {
+    suspend fun getUserAccountWithId(id: UserAccountId): UserAccount
 }
