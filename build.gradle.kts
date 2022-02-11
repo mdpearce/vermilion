86e4dfc9b490eb57e95ca3c8ac695249c3b157d9
Michael Pearce <mdpearce@gmail.com>
2022-02-11 19:42:14 +1100

    Configure releases hub with local properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-@file:Suppress("UnstableApiUsage")
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
@@ -19,7 +17,6 @@ buildscript {
 }
 
 plugins {
-    id("org.jmailen.kotlinter").version("3.8.0")
     id("com.dipien.releaseshub.gradle.plugin").version("3.1.0")
 }
 
@@ -31,6 +28,33 @@ tasks.create("clean", Delete::class) {
     this.setDelete(rootProject.buildDir)
 }
 
+fun getLocalProperties(): java.util.Properties {
+    val props = java.util.Properties()
+    if (file("custom.local.properties").exists()) {
+        props.load(java.io.FileInputStream(file("custom.local.properties")))
+    }
+    return props
+}
+
+fun getEnvVarOrLocalProperty(propertyName: String): String {
+    val envVar = System.getenv(propertyName)
+    return if (envVar == null || envVar.isEmpty()) {
+        getLocalProperties().getProperty(propertyName)
+    } else {
+        envVar
+    }
+}
+
 releasesHub {
     dependenciesPaths = listOf("buildSrc/src/main/kotlin/Deps.kt")
+    excludes = listOf("com.squareup.okhttp3")
+
+    pullRequestEnabled = true
+    pullRequestsMax = 15
+    gitHubRepositoryOwner = getEnvVarOrLocalProperty("GH_REPO_OWNER")
+    gitHubRepositoryName = getEnvVarOrLocalProperty("GH_REPO_NAME")
+    gitHubWriteToken = getEnvVarOrLocalProperty("GH_WRITE_TOKEN")
+    gitUserName = getEnvVarOrLocalProperty("GIT_USER_NAME")
+    gitUserEmail = getEnvVarOrLocalProperty("GIT_USER_EMAIL")
+    baseBranch = "main"
 }
