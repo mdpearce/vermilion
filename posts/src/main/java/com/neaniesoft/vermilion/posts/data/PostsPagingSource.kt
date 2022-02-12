3b75b1865f691cbca8121af4db6a42a4d8fa3a0a
Michael Pearce <mdpearce@gmail.com>
2022-02-12 18:38:50 +1100

    Use kotlin-result for error handling in paging source

--- posts/src/main/java/com/neaniesoft/vermilion/posts/data/PostsPagingSource.kt
+++ posts/src/main/java/com/neaniesoft/vermilion/posts/data/PostsPagingSource.kt
@@ -2,37 +2,43 @@ package com.neaniesoft.vermilion.posts.data
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.github.michaelbull.result.get
+import com.github.michaelbull.result.getError
+import com.github.michaelbull.result.map
 import com.neaniesoft.vermilion.posts.data.entities.PagingQuery
 import com.neaniesoft.vermilion.posts.domain.entities.Community
 import com.neaniesoft.vermilion.posts.domain.entities.FirstSet
 import com.neaniesoft.vermilion.posts.domain.entities.Post
-import retrofit2.HttpException
-import java.io.IOException
 
 class PostsPagingSource constructor(
     private val postRepository: PostRepository,
     private val community: Community
 ) : PagingSource<PagingQuery, Post>() {
     override suspend fun load(params: LoadParams<PagingQuery>): LoadResult<PagingQuery, Post> {
-        try {
-            val query = params.key ?: PagingQuery(FirstSet, 0)
-            val response = postRepository.postsForCommunity(
-                community = community,
-                requestedCount = params.loadSize,
-                previousCount = query.previousCount,
-                listingKey = query.listingKey
-            )
-
-            return LoadResult.Page(
+        val query = params.key ?: PagingQuery(FirstSet, 0)
+        val response = postRepository.postsForCommunity(
+            community = community,
+            requestedCount = params.loadSize,
+            previousCount = query.previousCount,
+            listingKey = query.listingKey
+        ).map { response ->
+            LoadResult.Page(
                 data = response.results,
                 prevKey = null,
-                nextKey = response.afterKey?.let { query.copy(listingKey = it, previousCount = query.previousCount + response.results.size) }
+                nextKey = response.afterKey?.let {
+                    query.copy(
+                        listingKey = it,
+                        previousCount = query.previousCount + response.results.size
+                    )
+                }
             )
-        } catch (e: IOException) {
-            return LoadResult.Error(e)
-        } catch (e: HttpException) {
-            return LoadResult.Error(e)
         }
+
+        return response.get()
+            ?: LoadResult.Error(
+                response.getError()
+                    ?: IllegalStateException("No error returned, but not value either")
+            )
     }
 
     override fun getRefreshKey(state: PagingState<PagingQuery, Post>): PagingQuery {
