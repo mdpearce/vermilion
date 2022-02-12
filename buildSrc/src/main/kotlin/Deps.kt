09000f1f67ba0460ae1d1e953b1e9066c339c13b
Michael <mdpearce@gmail.com>
2022-02-12 12:10:54 +0000

    Upgraded androidx.compose.ui:ui-test-junit4 from 1.1.0-rc03 to 1.1.0

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -26,7 +26,7 @@ object Deps {
     const val COMPOSE_UI_TOOLING = "androidx.compose.ui:ui-tooling:1.1.0"
     const val ANDROIDX_NAVIGATION_COMPOSE = "androidx.navigation:navigation-compose:2.5.0-alpha01"
     const val ANDROIDX_ACTIVITY_COMPOSE = "androidx.activity:activity-compose:1.5.0-alpha01"
-    const val COMPOSE_UI_TEST_JUNIT4 = "androidx.compose.ui:ui-test-junit4:1.1.0-rc03"
+    const val COMPOSE_UI_TEST_JUNIT4 = "androidx.compose.ui:ui-test-junit4:1.1.0"
     const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:7.1.0"
     const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10"
     const val HILT_ANDROID_GRADLE_PLUGIN = "com.google.dagger:hilt-android-gradle-plugin:2.40.5"
