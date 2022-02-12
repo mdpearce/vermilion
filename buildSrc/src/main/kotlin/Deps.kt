c8c527b43f3615ab418c8539a9ded0037ad005e5
Michael <mdpearce@gmail.com>
2022-02-12 12:10:54 +0000

    Upgraded androidx.compose.ui:ui-tooling from 1.1.0-rc03 to 1.1.0

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -23,7 +23,7 @@ object Deps {
     const val MATERIAL = "com.google.android.material:material:1.5.0"
     const val COMPOSE_MATERIAL = "androidx.compose.material:material:1.1.0-rc03"
     const val COMPOSE_UI_TOOLING_PREVIEW = "androidx.compose.ui:ui-tooling-preview:1.1.0-rc03"
-    const val COMPOSE_UI_TOOLING = "androidx.compose.ui:ui-tooling:1.1.0-rc03"
+    const val COMPOSE_UI_TOOLING = "androidx.compose.ui:ui-tooling:1.1.0"
     const val ANDROIDX_NAVIGATION_COMPOSE = "androidx.navigation:navigation-compose:2.5.0-alpha01"
     const val ANDROIDX_ACTIVITY_COMPOSE = "androidx.activity:activity-compose:1.5.0-alpha01"
     const val COMPOSE_UI_TEST_JUNIT4 = "androidx.compose.ui:ui-test-junit4:1.1.0-rc03"
