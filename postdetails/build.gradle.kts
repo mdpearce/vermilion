15c6169242cdfef081a8124227665355fe8b125e
Michael Pearce <mdpearce@gmail.com>
2022-02-15 17:00:17 +1100

    Add post details module based on posts module

---
+++ postdetails/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("vermilion.android-module-conventions")
+}
+
+dependencies {
+    implementation(Deps.MATERIAL)
+    implementation(Deps.COMPOSE_UI)
+    implementation(Deps.COMPOSE_MATERIAL)
+    implementation(Deps.COMPOSE_UI_TOOLING_PREVIEW)
+    implementation(Deps.LIFECYCLE_RUNTIME)
+    implementation(Deps.RETROFIT)
+    implementation(Deps.JACKSON_ANNOTATIONS)
+    implementation(Deps.COIL)
+    implementation(Deps.COIL_COMPOSE)
+    implementation(Deps.COMMONS_TEXT)
+    implementation(Deps.PAGING_RUNTIME)
+    implementation(Deps.PAGING_COMPOSE)
+    implementation(project(":ui"))
+    implementation(project(":api"))
+    implementation(project(":utils"))
+    implementation(project(":auth"))
+    implementation(project(":dbentities"))
+    implementation(project(":db"))
+    debugImplementation(Deps.COMPOSE_UI_TOOLING)
+
+    implementation(Deps.ROOM_RUNTIME)
+    kapt(Deps.ROOM_COMPILER)
+    implementation(Deps.ROOM_KOTLIN)
+    testImplementation(Deps.ROOM_TESTING)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
 @file:Suppress("UnstableApiUsage")
 
+include(":postdetails")
+
+
 include(":dbentities")
 
 
