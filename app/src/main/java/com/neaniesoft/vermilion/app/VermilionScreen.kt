02d69f08cd05af97031e3a87c2d013e291797423
Michael Pearce <mdpearce@gmail.com>
2022-02-15 16:52:07 +1100

    Add details screen to enum

--- app/src/main/java/com/neaniesoft/vermilion/app/VermilionScreen.kt
+++ app/src/main/java/com/neaniesoft/vermilion/app/VermilionScreen.kt
@@ -2,12 +2,14 @@ package com.neaniesoft.vermilion.app
 
 enum class VermilionScreen {
     Posts,
+    PostDetails,
     MyAccount,
     CustomTab;
 
     companion object {
         fun fromRoute(route: String?): VermilionScreen = when (route?.substringBefore("/")) {
             Posts.name -> Posts
+            PostDetails.name -> PostDetails
             MyAccount.name -> MyAccount
             CustomTab.name -> CustomTab
             null -> Posts
