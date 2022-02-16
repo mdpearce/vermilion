44e72e66b226d8f63eb4f933765468f96d47d046
Michael Pearce <mdpearce@gmail.com>
2022-02-16 12:16:47 +1100

    Add comment DAO

---
+++ dbentities/src/main/java/com/neaniesoft/vermilion/dbentities/comments/CommentDao.kt
@@ -0,0 +1,31 @@
+package com.neaniesoft.vermilion.dbentities.comments
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface CommentDao {
+    @Query("SELECT * from comments WHERE postId == :postId")
+    suspend fun getAllForPost(postId: String): List<CommentRecord>
+
+    @Query("SELECT * from comments WHERE postId == :postId AND parentId == null")
+    suspend fun getAllTopLevelRecordsForPost(postId: String): List<CommentRecord>
+
+    @Query("SELECT * from comments WHERE path LIKE :commentPath || '%'")
+    suspend fun getAllDescendantsOfCommentWithPath(commentPath: String): List<CommentRecord>
+
+    @Insert
+    suspend fun insertAll(vararg comments: CommentRecord)
+
+    @Insert
+    suspend fun insertAll(comments: List<CommentRecord>)
+
+    @Update
+    suspend fun update(comment: CommentRecord)
+
+    @Delete
+    suspend fun deleteAllForPost(postId: String)
+}
