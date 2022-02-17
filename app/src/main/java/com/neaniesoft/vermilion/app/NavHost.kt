f59144fd745519b7ea10cdec7dfaaafec452130b
Michael Pearce <mdpearce@gmail.com>
2022-02-17 17:32:38 +1100

    Remove unnecesary parens

--- app/src/main/java/com/neaniesoft/vermilion/app/NavHost.kt
+++ app/src/main/java/com/neaniesoft/vermilion/app/NavHost.kt
@@ -37,7 +37,7 @@ fun VermilionNavHost(navController: NavHostController, modifier: Modifier = Modi
             "${VermilionScreen.PostDetails.name}/{id}",
             arguments = listOf(navArgument("id") { type = NavType.StringType })
         ) {
-            PostDetailsScreen() {
+            PostDetailsScreen {
                 navController.navigate(customTabRoute(it))
             }
         }
