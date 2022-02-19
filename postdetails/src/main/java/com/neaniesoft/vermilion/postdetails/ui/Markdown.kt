6fd78a1c680756ca2a54dc7f0bcb98db1f6ba7f9
Michael Pearce <mdpearce@gmail.com>
2022-02-19 19:46:50 +1100

    Add markdown heading support

--- postdetails/src/main/java/com/neaniesoft/vermilion/postdetails/ui/Markdown.kt
+++ postdetails/src/main/java/com/neaniesoft/vermilion/postdetails/ui/Markdown.kt
@@ -1,6 +1,15 @@
 package com.neaniesoft.vermilion.postdetails.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.unit.dp
 import org.commonmark.node.BlockQuote
 import org.commonmark.node.BulletList
 import org.commonmark.node.Document
@@ -48,6 +57,36 @@ fun MarkdownThematicBreak(thematicBreak: ThematicBreak) {
 
 @Composable
 fun MarkdownHeading(heading: Heading) {
+    val style = when (heading.level) {
+        1 -> MaterialTheme.typography.h1
+        2 -> MaterialTheme.typography.h2
+        3 -> MaterialTheme.typography.h3
+        4 -> MaterialTheme.typography.h4
+        5 -> MaterialTheme.typography.h5
+        6 -> MaterialTheme.typography.h6
+        else -> {
+            // This isn't a header
+            MarkdownBlockChildren(parent = heading)
+            return
+        }
+    }
+
+    val padding = if (heading.parent is Document) 8.dp else 0.dp
+    Box(Modifier.padding(bottom = padding)) {
+        val text = buildAnnotatedString {
+            appendMarkdownChildren(heading, MaterialTheme.colors)
+        }
+        MarkdownText(text, style)
+    }
+}
+
+@Composable
+fun MarkdownText(text: AnnotatedString, style: TextStyle) {
+    TODO("Not yet implemented")
+}
+
+private fun AnnotatedString.Builder.appendMarkdownChildren(heading: Heading, colors: Colors) {
+    TODO("Not yet implemented")
 }
 
 @Composable
