814c21344d5502f5c12ae04fa8730d58fb285390
Michael Pearce <mdpearce@gmail.com>
2022-02-27 12:30:02 +1100

    Provide community DAO through DI

---
+++ communities/src/main/java/com/neaniesoft/vermilion/communities/data/database/CommunityDaoModule.kt
@@ -0,0 +1,15 @@
+package com.neaniesoft.vermilion.communities.data.database
+
+import com.neaniesoft.vermilion.db.VermilionDatabase
+import com.neaniesoft.vermilion.dbentities.communities.CommunityDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class CommunityDaoModule {
+    @Provides
+    fun provideCommunityDao(db: VermilionDatabase): CommunityDao = db.communityDao()
+}
