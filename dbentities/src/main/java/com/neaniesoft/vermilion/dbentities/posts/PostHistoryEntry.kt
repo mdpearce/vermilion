307ae3ce7e534cd1a76478ffa7ccc873120826fc
Michael Pearce <mdpearce@gmail.com>
2022-02-28 18:52:37 +1100

    Add post history entity table

---
+++ dbentities/src/main/java/com/neaniesoft/vermilion/dbentities/posts/PostHistoryEntry.kt
@@ -0,0 +1,12 @@
+package com.neaniesoft.vermilion.dbentities.posts
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "post_history")
+data class PostHistoryEntry(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val postId: String,
+    val visitedAt: Long
+)
