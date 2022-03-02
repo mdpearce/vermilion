28e3f7a2b7b9d004c20929f94ec4b682af9e52e4
Michael Pearce <mdpearce@gmail.com>
2022-03-02 17:55:11 +1100

    Add content for image dialog

--- ui/src/main/java/com/neaniesoft/vermilion/ui/images/ImageDialog.kt
+++ ui/src/main/java/com/neaniesoft/vermilion/ui/images/ImageDialog.kt
@@ -1,8 +1,20 @@
 package com.neaniesoft.vermilion.ui.images
 
 import android.net.Uri
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import coil.compose.rememberImagePainter
 
 @Composable
 fun ImageDialog(imageUri: Uri) {
+    Log.d("ImageDialog", "Loading image uri: $imageUri")
+    Surface(Modifier.fillMaxSize()) {
+        val painter = rememberImagePainter(imageUri)
+
+        Image(modifier = Modifier.fillMaxSize(), painter = painter, contentDescription = "Image")
+    }
 }
