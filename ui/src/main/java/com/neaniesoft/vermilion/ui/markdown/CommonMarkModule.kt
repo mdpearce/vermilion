35e25ba7cf71667f4c2c1705f97da65af1fd8206
Michael Pearce <mdpearce@gmail.com>
2022-03-05 19:28:22 +1100

    Add autolink extension to parser

--- ui/src/main/java/com/neaniesoft/vermilion/ui/markdown/CommonMarkModule.kt
+++ ui/src/main/java/com/neaniesoft/vermilion/ui/markdown/CommonMarkModule.kt
@@ -4,11 +4,19 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dagger.multibindings.IntoSet
+import org.commonmark.Extension
+import org.commonmark.ext.autolink.AutolinkExtension
 import org.commonmark.parser.Parser
 
 @Module
 @InstallIn(SingletonComponent::class)
 class CommonMarkModule {
     @Provides
-    fun provideParser(): Parser = Parser.builder().build()
+    fun provideParser(extensions: Set<@JvmSuppressWildcards Extension>): Parser =
+        Parser.builder().extensions(extensions).build()
+
+    @Provides
+    @IntoSet
+    fun provideAutolinkExtension(): Extension = AutolinkExtension.create()
 }
