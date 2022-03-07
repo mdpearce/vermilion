5577aa2315528616e8dd1b534b7044ace220c43f
Michael Pearce <mdpearce@gmail.com>
2022-03-07 18:21:16 +1100

    Revamp video player to hoist state

--- ui/src/main/java/com/neaniesoft/vermilion/ui/videos/VideoDialog.kt
+++ ui/src/main/java/com/neaniesoft/vermilion/ui/videos/VideoDialog.kt
@@ -1,8 +1,10 @@
 package com.neaniesoft.vermilion.ui.videos
 
 import android.util.Log
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.neaniesoft.vermilion.ui.images.rememberZoomableState
 
 @ExperimentalMaterialApi
@@ -10,7 +12,12 @@ import com.neaniesoft.vermilion.ui.images.rememberZoomableState
 fun VideoDialog(videoDescriptor: VideoDescriptor, onDismiss: () -> Unit) {
     Log.d("VideoDialog", "Loading video: $videoDescriptor")
     val zoomableState = rememberZoomableState(maxScale = 6f)
+    val videoPlayerState = rememberVideoPlayerState()
     ZoomableDialog(state = zoomableState, onDismiss = onDismiss) {
-        VideoPlayer(video = videoDescriptor)
+        VideoPlayer(
+            state = videoPlayerState,
+            video = videoDescriptor,
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
--- ui/src/main/java/com/neaniesoft/vermilion/ui/videos/VideoPlayer.kt
+++ ui/src/main/java/com/neaniesoft/vermilion/ui/videos/VideoPlayer.kt
@@ -2,11 +2,16 @@ package com.neaniesoft.vermilion.ui.videos
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
@@ -18,45 +23,66 @@ import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.StyledPlayerView
 
 @Composable
-fun VideoPlayer(video: VideoDescriptor) {
+fun rememberVideoPlayerState(
+    initialAutoPlay: Boolean = true,
+    initialPosition: Long = 0L
+): VideoPlayerState = rememberSaveable(
+    saver = VideoPlayerState.Saver
+) {
+    VideoPlayerState(
+        initialAutoPlay, initialPosition
+    )
+}
+
+@Stable
+class VideoPlayerState(
+    initialAutoPlay: Boolean = true,
+    initialPosition: Long = 0L
+) {
+    var autoPlay by mutableStateOf(initialAutoPlay)
+        private set
+    var position by mutableStateOf(initialPosition)
+        private set
+
+    internal fun onUpdatePositionFromPlayer(newPos: Long) {
+        position = newPos
+    }
+
+    internal fun onUpdateAutoPlayStateFromPlayer(newValue: Boolean) {
+        autoPlay = newValue
+    }
+
+    companion object {
+        val Saver: Saver<VideoPlayerState, *> = listSaver(
+            save = {
+                listOf(
+                    it.autoPlay,
+                    it.position
+                )
+            },
+            restore = {
+                VideoPlayerState(it[0] as Boolean, it[1] as Long)
+            }
+        )
+    }
+}
+
+@Composable
+fun VideoPlayer(state: VideoPlayerState, video: VideoDescriptor, modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val lifecycle = LocalLifecycleOwner.current.lifecycle
 
-    // TODO This should be hoisted out to a state object for observation
-    var autoPlay by rememberSaveable { mutableStateOf(true) }
-    var window by rememberSaveable { mutableStateOf(0) }
-    var position by rememberSaveable { mutableStateOf(0L) }
-
     val player = remember {
-        ExoPlayer.Builder(context).build().apply {
+        ExoPlayer.Builder(context).build()
+    }
+
+    LaunchedEffect(key1 = video) {
+        player.apply {
+            repeatMode = Player.REPEAT_MODE_ONE
+            playWhenReady = state.autoPlay
             setMediaItem(MediaItem.fromUri(video.dash))
             prepare()
-            repeatMode = Player.REPEAT_MODE_ONE
-            playWhenReady = autoPlay
-            seekTo(window, position)
-        }
-    }
-
-    fun updateState() {
-        autoPlay = player.playWhenReady
-        window = player.currentMediaItemIndex
-        position = 0L.coerceAtLeast(player.contentPosition) // ??
-    }
-
-    val playerView = remember {
-        StyledPlayerView(context).also { view ->
-            lifecycle.addObserver(object : DefaultLifecycleObserver {
-                override fun onStart(owner: LifecycleOwner) {
-                    view.onResume()
-                    player.playWhenReady = autoPlay
-                }
-
-                override fun onStop(owner: LifecycleOwner) {
-                    updateState()
-                    view.onPause()
-                    player.playWhenReady = false
-                }
-            })
+            seekTo(state.position)
         }
     }
 
@@ -66,9 +92,26 @@ fun VideoPlayer(video: VideoDescriptor) {
         }
     }
 
-    AndroidView(factory = {
-        playerView
-    }, update = {
-        playerView.player = player
-    })
+    AndroidView(
+        factory = {
+            StyledPlayerView(it)
+        },
+        modifier = modifier
+    ) { playerView ->
+        lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onStart(owner: LifecycleOwner) {
+                playerView.onResume()
+            }
+
+            override fun onStop(owner: LifecycleOwner) {
+                state.onUpdateAutoPlayStateFromPlayer(player.playWhenReady)
+                state.onUpdatePositionFromPlayer(player.currentPosition)
+                playerView.onPause()
+            }
+        })
+
+        playerView.apply {
+            this.player = player
+        }
+    }
 }
