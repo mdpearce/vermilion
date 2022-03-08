b672fff10329c110b07f4ecc2fa4072a251916e7
Michael Pearce <mdpearce@gmail.com>
2022-03-08 19:08:08 +1100

    Call through the resolvers to find one that handles the Uri, then resolve it

--- ui/src/main/java/com/neaniesoft/vermilion/ui/videos/ExternalVideoDialog.kt
+++ ui/src/main/java/com/neaniesoft/vermilion/ui/videos/ExternalVideoDialog.kt
@@ -5,12 +5,16 @@ import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.onFailure
+import com.github.michaelbull.result.onSuccess
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -20,20 +24,51 @@ fun ExternalVideoDialog(viewModel: ExternalVideoDialogViewModel = hiltViewModel(
 
 interface VideoUriResolver {
     suspend fun resolve(uri: Uri): Result<Uri, VideoResolverErrors>
+    fun handles(uri: Uri): Boolean
 }
 
 sealed class VideoResolverErrors
+object NoRegisteredResolvers : VideoResolverErrors()
 
 @HiltViewModel
 class ExternalVideoDialogViewModel @Inject constructor(
     private val resolvers: Set<@JvmSuppressWildcards VideoUriResolver>
 ) : ViewModel() {
+    private val _uiState =
+        MutableStateFlow<ExternalVideoDialogState>(ExternalVideoDialogState.Loading)
+    val uiState = _uiState.asStateFlow()
 
+    suspend fun onResolveExternalUri(uri: Uri) {
+        _uiState.emit(ExternalVideoDialogState.Loading)
+
+        val resolver = resolvers.firstOrNull { it.handles(uri) }
+        if (resolver != null) {
+            resolver.resolve(uri)
+                .onSuccess {
+                    _uiState.emit(ExternalVideoDialogState.PlayUriState(it))
+                }
+                .onFailure {
+                    _uiState.emit(ExternalVideoDialogState.ErrorState(it))
+                }
+        } else {
+            _uiState.emit(ExternalVideoDialogState.ErrorState(NoRegisteredResolvers))
+        }
+    }
+}
+
+sealed class ExternalVideoDialogState {
+    object Loading : ExternalVideoDialogState()
+    data class ErrorState(val error: VideoResolverErrors) : ExternalVideoDialogState()
+    data class PlayUriState(val uri: Uri) : ExternalVideoDialogState()
 }
 
 @Singleton
 class RedGifsVideoUriResolver @Inject constructor() : VideoUriResolver {
     override suspend fun resolve(uri: Uri): Result<Uri, VideoResolverErrors> {
+        TODO("Not yet implemented")
+    }
+
+    override fun handles(uri: Uri): Boolean {
         TODO("Not yet implemented")
     }
 }
