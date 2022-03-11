08dbaf3b0747d3e9b0619fbf6c89f7b0306e97f4
Michael Pearce <mdpearce@gmail.com>
2022-03-11 18:58:40 +1100

    Add play/pause button

--- ui/src/main/java/com/neaniesoft/vermilion/ui/videos/direct/VideoDialog.kt
+++ ui/src/main/java/com/neaniesoft/vermilion/ui/videos/direct/VideoDialog.kt
@@ -1,17 +1,24 @@
 package com.neaniesoft.vermilion.ui.videos.direct
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.google.android.exoplayer2.MediaItem
+import com.neaniesoft.vermilion.ui.R
 import com.neaniesoft.vermilion.ui.images.rememberZoomableState
 import com.neaniesoft.vermilion.ui.videos.ZoomableDialog
 import com.neaniesoft.vermilion.ui.videos.exoplayer.ExoPlayer
@@ -32,16 +39,28 @@ fun VideoDialog(videoDescriptor: VideoDescriptor, onDismiss: () -> Unit) {
         Box(contentAlignment = Alignment.Center) {
             ExoPlayer(exoPlayerState)
             Box(
-                contentAlignment = Alignment.BottomEnd,
+                contentAlignment = Alignment.BottomCenter,
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp)
             ) {
-                VideoCounter(
-                    exoPlayerState.secondsRemaining
+                VideoControlRow(
+                    secondsRemaining = exoPlayerState.secondsRemaining,
+                    isPlaying = exoPlayerState.playWhenReady,
+                    onPlayPauseClick = {
+                        exoPlayerState.playWhenReady = !exoPlayerState.playWhenReady
+                    }
                 )
             }
         }
+    }
+}
+
+@Composable
+fun VideoControlRow(secondsRemaining: Long, isPlaying: Boolean, onPlayPauseClick: () -> Unit) {
+    Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+        PlayPauseButton(isPlaying = isPlaying, onPlayPauseClick)
+        VideoCounter(secondsRemaining = secondsRemaining)
     }
 }
 
@@ -51,5 +70,27 @@ fun VideoCounter(secondsRemaining: Long) {
     val timeRemainingString = "%d:%02d".format(
         duration.inWholeMinutes,
         duration.toComponents { _, seconds, _ -> seconds })
-    Text(text = timeRemainingString, modifier = Modifier.padding(8.dp), style = MaterialTheme.typography.body1)
+    Text(
+        text = timeRemainingString,
+        modifier = Modifier.padding(8.dp),
+        style = MaterialTheme.typography.body1
+    )
+}
+
+@Composable
+fun PlayPauseButton(
+    isPlaying: Boolean,
+    onClick: () -> Unit
+) {
+    val iconResource = painterResource(
+        id = if (isPlaying) {
+            R.drawable.ic_baseline_pause_circle_filled_24
+        } else {
+            R.drawable.ic_baseline_play_circle_filled_24
+        }
+    )
+
+    IconButton(onClick = onClick) {
+        Icon(painter = iconResource, contentDescription = "Play/Pause")
+    }
 }
