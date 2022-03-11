6b945cf156ce575f731d247470cf606666e50b19
Michael Pearce <mdpearce@gmail.com>
2022-03-11 12:41:07 +1100

    Add effect to start polling for position updates

--- ui/src/main/java/com/neaniesoft/vermilion/ui/videos/exoplayer/ExoPlayer.kt
+++ ui/src/main/java/com/neaniesoft/vermilion/ui/videos/exoplayer/ExoPlayer.kt
@@ -1,6 +1,7 @@
 package com.neaniesoft.vermilion.ui.videos.exoplayer
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
@@ -12,22 +13,34 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
 import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.Player.STATE_READY
+import com.google.android.exoplayer2.Timeline
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.neaniesoft.vermilion.utils.anonymousLogger
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 
 @Composable
 fun rememberExoPlayerState(
     initialMediaItem: MediaItem? = null,
     initialPosition: Long = 0,
     initialPlayWhenReady: Boolean = true,
-    initialRepeatMode: Int = Player.REPEAT_MODE_OFF
+    initialRepeatMode: Int = Player.REPEAT_MODE_OFF,
+    initialMuteState: Boolean = false
 ): ExoPlayerState = rememberSaveable(saver = ExoPlayerState.Saver) {
     ExoPlayerState(
         initialMediaItem = initialMediaItem,
         initialPosition = initialPosition,
         initialPlayWhenReady = initialPlayWhenReady,
-        initialRepeatMode = initialRepeatMode
+        initialRepeatMode = initialRepeatMode,
+        initialMuteState = initialMuteState
     )
 }
 
@@ -36,7 +49,8 @@ class ExoPlayerState(
     initialMediaItem: MediaItem? = null,
     initialPosition: Long = 0,
     initialPlayWhenReady: Boolean = true,
-    initialRepeatMode: Int = Player.REPEAT_MODE_OFF
+    initialRepeatMode: Int = Player.REPEAT_MODE_OFF,
+    initialMuteState: Boolean = false
 ) {
     var mediaItem: MediaItem? by mutableStateOf(initialMediaItem)
 
@@ -48,20 +62,15 @@ class ExoPlayerState(
     var playbackState: Int by mutableStateOf(Player.STATE_IDLE)
         private set
 
-    var isPlaying: Boolean by mutableStateOf(false)
-        private set
-
     var duration: Long by mutableStateOf(C.TIME_UNSET)
         private set
 
     var repeatMode: Int by mutableStateOf(initialRepeatMode)
 
+    var isMuted: Boolean by mutableStateOf(initialMuteState)
+
     internal fun onDurationUpdated(newDuration: Long) {
         duration = newDuration
-    }
-
-    internal fun onIsPlayingUpdated(newValue: Boolean) {
-        isPlaying = newValue
     }
 
     internal fun onPlaybackStateChanged(newState: Int) {
@@ -79,7 +88,8 @@ class ExoPlayerState(
                     state.mediaItem,
                     state.position,
                     state.playWhenReady,
-                    state.repeatMode
+                    state.repeatMode,
+                    state.isMuted
                 )
             },
             restore = {
@@ -87,7 +97,8 @@ class ExoPlayerState(
                     initialMediaItem = it[0] as MediaItem?,
                     initialPosition = it[1] as Long,
                     initialPlayWhenReady = it[2] as Boolean,
-                    initialRepeatMode = it[3] as Int
+                    initialRepeatMode = it[3] as Int,
+                    initialMuteState = it[4] as Boolean
                 )
             }
         )
@@ -96,26 +107,77 @@ class ExoPlayerState(
 
 @Composable
 fun ExoPlayer(state: ExoPlayerState = rememberExoPlayerState()) {
+    val logger by anonymousLogger("ExoPlayerComposable")
     val context = LocalContext.current
     val lifecycle = LocalLifecycleOwner.current.lifecycle
 
-    val exoPlayer = remember {
+    val player: Player = remember {
         com.google.android.exoplayer2.ExoPlayer.Builder(context).build()
+    }
+
+    LaunchedEffect(Unit) {
+        player.addListener(object : Player.Listener {
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                logger.debugIfEnabled { "Playback state changed: $playbackState" }
+                state.onPlaybackStateChanged(playbackState)
+            }
+
+            override fun onTimelineChanged(timeline: Timeline, reason: Int) {
+                logger.debugIfEnabled { "Timeline changed. New duration: ${player.duration}" }
+                state.onDurationUpdated(player.duration)
+            }
+
+            override fun onPlayerError(error: PlaybackException) {
+                logger.errorIfEnabled(error) { "Error playing video" }
+            }
+        })
+    }
+
+    LaunchedEffect(state.playbackState) {
+        if (state.playbackState == STATE_READY) {
+            logger.debugIfEnabled { "Playback State is READY. Starting polling for position" }
+            while (isActive) {
+                state.onPositionChanged(player.currentPosition)
+                delay(100)
+            }
+        }
     }
 
     LaunchedEffect(state.mediaItem) {
         val mediaItem = state.mediaItem
         if (mediaItem != null) {
-            exoPlayer.setMediaItem(mediaItem)
-            exoPlayer.seekTo(state.position)
+            player.setMediaItem(mediaItem)
+            player.prepare()
+            player.seekTo(state.position)
         }
     }
 
     LaunchedEffect(state.repeatMode) {
-        exoPlayer.repeatMode = state.repeatMode
+        player.repeatMode = state.repeatMode
     }
 
     LaunchedEffect(state.playWhenReady) {
-        exoPlayer.playWhenReady = state.playWhenReady
+        player.playWhenReady = state.playWhenReady
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            player.release()
+        }
+    }
+
+
+    AndroidView(factory = {
+        StyledPlayerView(context)
+    }) { playerView ->
+        lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onStart(owner: LifecycleOwner) {
+                playerView.onPause()
+            }
+
+            override fun onStop(owner: LifecycleOwner) {
+                playerView.onResume()
+            }
+        })
     }
 }
