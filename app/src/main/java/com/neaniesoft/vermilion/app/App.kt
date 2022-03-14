b8132ca957e1926d47c60f41ba1b9da1c756089c
Michael Pearce <mdpearce@gmail.com>
2022-03-14 18:49:44 +1100

    Put my account button in top app bar

--- app/src/main/java/com/neaniesoft/vermilion/app/App.kt
+++ app/src/main/java/com/neaniesoft/vermilion/app/App.kt
@@ -5,9 +5,13 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,6 +22,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -120,7 +125,15 @@ fun VermilionApp(
                 TopAppBar(
                     elevation = 16.dp,
                     title = { Text(currentScreen.name) },
-                    modifier = Modifier.clickable { scope.launch { appState.onAppBarClicked() } }
+                    modifier = Modifier.clickable { scope.launch { appState.onAppBarClicked() } },
+                    actions = {
+                        IconButton(onClick = { navController.navigate("${VermilionScreen.MyAccount}") }) {
+                            Icon(
+                                Icons.Default.Person,
+                                contentDescription = stringResource(id = R.string.content_description_my_account_button)
+                            )
+                        }
+                    }
                 )
             },
             bottomBar = {
