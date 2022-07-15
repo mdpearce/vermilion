c5f362ccfbe43d01462b1b1df74e1ce8740204f7
Michael <mdpearce@gmail.com>
2022-07-15 12:12:52 +0000

    Upgraded com.google.dagger:hilt-compiler from 2.41 to 2.42

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,5 +1,5 @@
 object Deps {
-    const val HILT_COMPILER = "com.google.dagger:hilt-compiler:2.41"
+    const val HILT_COMPILER = "com.google.dagger:hilt-compiler:2.42"
     const val HILT_ANDROID = "com.google.dagger:hilt-android:2.41"
     const val HILT_ANDROID_TESTING = "com.google.dagger:hilt-android-testing:2.42"
     const val LIFECYCLE_VIEWMODEL_COMPOSE =
