0d68f02ccb76094cbf6d1849845756133d637012
Michael <mdpearce@gmail.com>
2022-08-27 12:18:42 +0000

    Upgraded com.google.accompanist:accompanist-pager from 0.23.1 to 0.25.1

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -63,6 +63,6 @@ object Deps {
         "org.jetbrains.kotlin:kotlin-serialization:1.6.10"
     const val KOTLINX_SERIALIZATION_JSON = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0"
     const val ANDROID_YOUTUBE_PLAYER = "com.pierfrancescosoffritti.androidyoutubeplayer:core:11.0.1"
-    const val ACCOMPANIST_PAGER = "com.google.accompanist:accompanist-pager:0.23.1"
+    const val ACCOMPANIST_PAGER = "com.google.accompanist:accompanist-pager:0.25.1"
     const val ACCOMPANIST_PAGER_INDICATORS = "com.google.accompanist:accompanist-pager-indicators:0.23.1"
 }
