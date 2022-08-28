c06c593deb09f83fc59e3824aad89ab4d4a8c04e
Michael Pearce <mdpearce@gmail.com>
2022-08-28 16:05:46 +1000

    Upgrade hilt plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,5 +12,5 @@ dependencies {
     //TODO: Replace these hardcoded values with centralized dependencies
     implementation("com.android.library:com.android.library.gradle.plugin:7.2.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-    implementation("com.google.dagger:hilt-android-gradle-plugin:2.42")
+    implementation("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
 }
