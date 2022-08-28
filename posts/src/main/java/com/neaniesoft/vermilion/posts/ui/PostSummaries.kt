dca238ead399000181ab8e37cc1465cb9742c843
Michael Pearce <mdpearce@gmail.com>
2022-08-28 13:57:22 +1000

    Refactor ImageSummary to use AsyncImage

--- posts/src/main/java/com/neaniesoft/vermilion/posts/ui/PostSummaries.kt
+++ posts/src/main/java/com/neaniesoft/vermilion/posts/ui/PostSummaries.kt
@@ -1,6 +1,5 @@
 package com.neaniesoft.vermilion.posts.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
@@ -11,11 +10,12 @@ import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
-import coil.transform.BlurTransformation
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.neaniesoft.vermilion.coreentities.UriImage
 import com.neaniesoft.vermilion.posts.R
 import com.neaniesoft.vermilion.ui.markdown.MarkdownDocument
@@ -42,23 +42,25 @@ fun TextSummary(
 
 @Composable
 fun ImageSummary(image: UriImage, isNsfw: Boolean, onClick: () -> Unit) {
-    val painter = rememberImagePainter(data = image.uri.toString()) {
-        placeholder(R.drawable.image_placeholder)
-        if (isNsfw) {
-            transformations(BlurTransformation(LocalContext.current, 25f, 8f))
-        }
-        crossfade(200)
-    }
-
     BoxWithConstraints(Modifier.fillMaxWidth()) {
         val ratio = remember {
             image.width.toFloat() / image.height.toFloat()
         }
-        Image(
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(image.uri.toString())
+                .placeholder(R.drawable.image_placeholder)
+                .crossfade(200)
+                .build(),
             modifier = Modifier
                 .size(maxWidth, maxWidth.div(ratio))
-                .clickable { onClick() },
-            painter = painter,
+                .clickable { onClick() }
+                .also {
+                    if (isNsfw) {
+                        it.blur(24.dp)
+                    }
+                }
+                .blur(24.dp),
             contentDescription = "",
             contentScale = ContentScale.Crop
         )
