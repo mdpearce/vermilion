f4e2da24a6ef8177badc7cbdb20262e39090c8e8
Michael Pearce <mdpearce@gmail.com>
2022-09-04 15:07:49 +1000

    Migrate post history from room to sqldelight

--- posts/src/main/java/com/neaniesoft/vermilion/posts/data/database/PostHistoryDaoModule.kt
+++ posts/src/main/java/com/neaniesoft/vermilion/posts/data/database/PostHistoryDaoModule.kt
@@ -1,11 +1,14 @@
 package com.neaniesoft.vermilion.posts.data.database
 
+import com.neaniesoft.vermilion.db.Database
+import com.neaniesoft.vermilion.db.PostHistoryQueries
 import com.neaniesoft.vermilion.db.VermilionDatabase
 import com.neaniesoft.vermilion.dbentities.posts.PostHistoryDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -13,4 +16,9 @@ class PostHistoryDaoModule {
     @Provides
     fun providePostHistoryDao(database: VermilionDatabase): PostHistoryDao =
         database.postHistoryDao()
+
+    @Provides
+    @Singleton
+    fun providePostHistoryQueries(database: Database): PostHistoryQueries =
+        database.postHistoryQueries
 }
--- posts/src/main/java/com/neaniesoft/vermilion/posts/data/database/PostHistoryRoomRepository.kt
+++ posts/src/main/java/com/neaniesoft/vermilion/posts/data/database/PostHistoryRoomRepository.kt
@@ -1,40 +1,29 @@
 package com.neaniesoft.vermilion.posts.data.database
 
 import androidx.room.withTransaction
+import com.neaniesoft.vermilion.db.PostHistoryQueries
 import com.neaniesoft.vermilion.db.VermilionDatabase
 import com.neaniesoft.vermilion.dbentities.posts.PostHistoryDao
 import com.neaniesoft.vermilion.dbentities.posts.PostHistoryRecord
 import com.neaniesoft.vermilion.posts.data.PostHistoryRepository
 import com.neaniesoft.vermilion.posts.domain.entities.PostHistoryEntry
 import com.neaniesoft.vermilion.posts.domain.entities.PostId
+import com.squareup.sqldelight.runtime.coroutines.asFlow
 import java.time.Instant
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class PostHistoryRoomRepository @Inject constructor(
-    private val db: VermilionDatabase,
-    private val dao: PostHistoryDao
+    private val queries: PostHistoryQueries
 ) : PostHistoryRepository {
     override suspend fun addHistoryEntry(entry: PostHistoryEntry) {
-        db.withTransaction {
-            dao.insertAll(entry.toRecord())
-        }
+        queries.insert(null, entry.postId.value, entry.viewedAt.toEpochMilli())
     }
 
     override suspend fun allEntries(): List<PostHistoryEntry> {
-        return db.withTransaction {
-            dao.getAllRecordsByDate()
-        }.map { record ->
-            record.toEntry()
-        }
-    }
-
-    private fun PostHistoryRecord.toEntry(): PostHistoryEntry {
-        return PostHistoryEntry(PostId(postId), Instant.ofEpochMilli(visitedAt))
-    }
-
-    private fun PostHistoryEntry.toRecord(): PostHistoryRecord {
-        return PostHistoryRecord(0, postId.value, viewedAt.toEpochMilli())
+        return queries.historyRecordsByDate { id, post_id, visited_at ->
+            PostHistoryEntry(PostId((post_id)), Instant.ofEpochMilli(visited_at))
+        }.executeAsList()
     }
 }
