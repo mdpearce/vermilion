fcab090d615ce0492dca7073b15a252a27037089
Michael <mdpearce@gmail.com>
2022-09-16 12:12:11 +0000

    Upgraded com.google.firebase:firebase-crashlytics-gradle from 2.9.1 to 2.9.2

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -51,7 +51,7 @@ object Deps {
     const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics-ktx"
     const val FIREBASE_CRASHLYTICS = "com.google.firebase:firebase-crashlytics-ktx"
     const val FIREBASE_CRASHLYTICS_GRADLE_PLUGIN =
-        "com.google.firebase:firebase-crashlytics-gradle:2.9.1"
+        "com.google.firebase:firebase-crashlytics-gradle:2.9.2"
     const val ACCOMPANIST_SWIPE_REFRESH = "com.google.accompanist:accompanist-swiperefresh:0.25.1"
     const val EXOPLAYER = "com.google.android.exoplayer:exoplayer:2.18.1"
     const val KOTLINX_SERIALIZATION_GRADLE_PLUGIN =
