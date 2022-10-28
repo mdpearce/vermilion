30bfad81c7e194865b046375bc785086e6019246
Michael <mdpearce@gmail.com>
2022-10-28 12:10:49 +0000

    Upgraded com.google.firebase:firebase-bom from 31.0.1 to 31.0.2

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -47,7 +47,7 @@ object Deps {
     const val COMMONMARK_EXT_AUTOLINK = "org.commonmark:commonmark-ext-autolink:0.19.0"
     const val SEMVER = "net.thauvin.erik.gradle:semver:1.0.4"
     const val GOOGLE_SERVICES_PLUGIN = "com.google.gms:google-services:4.3.13"
-    const val FIREBASE_BOM = "com.google.firebase:firebase-bom:31.0.1"
+    const val FIREBASE_BOM = "com.google.firebase:firebase-bom:31.0.2"
     const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics-ktx"
     const val FIREBASE_CRASHLYTICS = "com.google.firebase:firebase-crashlytics-ktx"
     const val FIREBASE_CRASHLYTICS_GRADLE_PLUGIN =
