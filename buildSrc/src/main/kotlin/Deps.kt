dbb99234998997b0768491042875bda3921d63b6
Michael <mdpearce@gmail.com>
2022-10-29 12:12:39 +0000

    Upgraded androidx.navigation:navigation-compose from 2.5.2 to 2.5.3

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -25,7 +25,7 @@ object Deps {
     const val COMPOSE_MATERIAL = "androidx.compose.material:material:1.2.1"
     const val COMPOSE_UI_TOOLING_PREVIEW = "androidx.compose.ui:ui-tooling-preview:1.2.1"
     const val COMPOSE_UI_TOOLING = "androidx.compose.ui:ui-tooling:1.2.1"
-    const val ANDROIDX_NAVIGATION_COMPOSE = "androidx.navigation:navigation-compose:2.5.2"
+    const val ANDROIDX_NAVIGATION_COMPOSE = "androidx.navigation:navigation-compose:2.5.3"
     const val ANDROIDX_ACTIVITY_COMPOSE = "androidx.activity:activity-compose:1.5.1"
     const val COMPOSE_UI_TEST_JUNIT4 = "androidx.compose.ui:ui-test-junit4:1.2.1"
     const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:7.2.2"
